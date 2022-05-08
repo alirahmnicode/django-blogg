14939fd99721dba19ed1c3b202e0cadd06970adf
alirahmanicode <alirahmanicode@gmail.com>
2022-05-08 21:05:50 +0430

    update api views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.core.exceptions import PermissionDenied
+
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -25,17 +28,36 @@ class ArticleCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        print(serializer.validated_data)
-        serializer.save(user=self.request.user)
+        data = dict(serializer.validated_data)
+        user = self.request.user
+        slug = slugify(data['title'])
+        if user:
+            serializer.save(user=user,slug=slug)
+        else:
+            return Response('User not found ...',status=204)
 
 class ArticleUpdateView(generics.UpdateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
 
+    def perform_update(self, serializer):
+        obj = self.get_object()
+        if self.request.user != obj.user:
+            raise PermissionDenied()
+        serializer.save()
 
 class ArticleDeleteView(generics.DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        obj = self.get_object()
+
+        if self.request.user != obj.user:
+            raise PermissionDenied()
+        instance.delete()
 
 
 class AddingTagView(generics.CreateAPIView):
@@ -47,18 +69,16 @@ class AddingTagView(generics.CreateAPIView):
 class AddingLikeView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self,request,**kwargs):
-        user = request.user.id
+        user = request.user
         if user:
-            article_pk = kwargs['pk']
-            article = get_object_or_404(Article , pk=article_pk)
-            if user not in article.likes.all():
-                article.likes.add(user)
-                message = 'liked'
-            else:
+            article = get_object_or_404(Article , pk=kwargs['pk'])
+            if user in article.likes.all():
                 article.likes.remove(user)
                 message = 'unliked'
+            else:
+                article.likes.add(user)
+                message = 'liked'
             article.save()
-            
             return Response(message,status=200)
         else:
             return Response('user not found...' , status=404)
@@ -70,7 +90,6 @@ class FilterByTag(APIView):
         articles = Article.objects.filter(tags__tag__icontains=tag)
         serializer = ArticleSerializer(articles,many=True)
         return Response(serializer.data,status=200)
-
 
 
 class UsersListView(generics.ListAPIView):
