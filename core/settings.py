ed851facaac33b3eab94f9178a9314eb75e79337
alirahmanicode <alirahmanicode@gmail.com>
2022-05-16 12:00:17 +0430

    create collect static

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root/")
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 MEDIA_URL = "/media/"
