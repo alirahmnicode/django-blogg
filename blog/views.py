2bfd463a6b2344792cbf5b282f353dc15aef8f32
alirahmanicode <alirahmanicode@gmail.com>
2022-06-21 19:44:00 +0430

    fix dont saving tags with their objects bog

--- blog/views.py
+++ blog/views.py
@@ -72,6 +72,7 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
             obj = form.save(commit=False)            
             obj.user = request.user
             obj.save()
+            form.save_m2m()
             return redirect("/")
 
 
